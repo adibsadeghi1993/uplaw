a51741aac18d3acbca15222d85a2143f73f6e54c
adib sadeghi <adibsadeghi7272@gmail.com>
2021-08-18 00:18:22 +0430

    user step component functionality was completed

--- src/components/userInfo/commonFiles/userStep/UserStep.js
+++ src/components/userInfo/commonFiles/userStep/UserStep.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import {steps} from "./Stages"
-import "./userStep.css"
+import React, { useState, useEffect } from "react";
+import { steps } from "./Stages";
+import "./userStep.css";
 
+const UserStep = ({ step, setStep, progress, setProgress }) => {
+  const [stage, setStage] = useState(steps);
 
-const UserStep = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  
+  useEffect(() => {
+    const updatedStages = [...stage];
+    const findedStep = updatedStages[step - 1];
+    const newStep = { ...findedStep, background: "activeBackground" };
+    updatedStages[step - 1] = newStep;
+    setStage(updatedStages);
+  }, [step]);
+
+  const stepHandler=(item)=>{
+    console.log(item)
+    setStep(item.step)
+ 
 }
+  return (
+    <div>
+      <p className="stepParagraf">تکمیل پروفایل</p>
+      <section className="steps">
+        {stage.map((item) => {
+          return (
+            <button
+              key={item.step}
+              onClick={() => stepHandler(item)}
+              className={`${item.defaultClass} ${item.background}`}
+            >
+              {item.stepTitle}
+            </button>
+          );
+        })}
+      </section>
+    </div>
+  );
+};
 
-export default UserStep
+export default UserStep;
