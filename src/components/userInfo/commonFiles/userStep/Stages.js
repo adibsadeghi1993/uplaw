1a25eb55dd33aaba18e250ecca3ca3ba50d59ea6
adib sadeghi <adibsadeghi7272@gmail.com>
2021-08-18 00:01:46 +0430

    four step in stagesfile was created

---
+++ src/components/userInfo/commonFiles/userStep/Stages.js
@@ -0,0 +1,26 @@
+export const steps = [
+    {
+      step: 1,
+      stepTitle: "اطلاعات پایه",
+      defaultClass: "step firstStep",
+      background: "defaltBackground",
+    },
+    {
+        step: 2,
+        stepTitle: "مسائل شغلی",
+        defaultClass: "step stepTow",
+        background: "defaltBackground",
+      },
+      {
+        step: 3,
+        stepTitle: "راه های ارتباطی",
+        defaultClass: "step stepThree",
+        background: "defaltBackground",
+      },
+      {
+        step: 4,
+        stepTitle: "احراز هویت",
+        defaultClass: "step five",
+        background: "defaltBackground",
+      }
+  ]
