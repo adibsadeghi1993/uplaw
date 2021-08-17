c5b3fade01701f99fd4d6f4fb72de7bc643074c0
adib sadeghi <adibsadeghi7272@gmail.com>
2021-08-17 23:38:18 +0430

    useFormik was removed and imported in specsfic component

--- src/components/userInfo/basicInfo/BasicInfo.js
+++ src/components/userInfo/basicInfo/BasicInfo.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import UserProfile from '../commonFiles/userProfile/UserProfile'
-import DetailInfo from './detail/DetailInfo'
-import "./basicInfo.css"
+import React from "react";
+import UserProfile from "../commonFiles/userProfile/UserProfile";
+import DetailInfo from "./detail/DetailInfo";
+import "./basicInfo.css";
 
 const BasicInfo = () => {
-    return (
-        <div className="basicInfo">
-        <section className="userprofile"> <UserProfile/></section>
-         <section className="detailinfo"><DetailInfo/></section>
-        </div>
-    )
-}
+  return (
+    <div className="basicInfo">
+      <section className="userprofile">
+        <UserProfile />
+      </section>
+      <section className="detailinfo">
+        <DetailInfo />
+      </section>
+    </div>
+  );
+};
 
-export default BasicInfo
+export default BasicInfo;
--- src/pages/Dashbord.js
+++ src/pages/Dashbord.js
@@ -9,13 +9,13 @@ const Dashbord = () => {
     const [step, setStep] = useState(1);
     switch (step) {
         case 1:
-          return <BasicInfo  setStep={setStep} step={step} useFormik={useFormik} />;
+          return <BasicInfo  setStep={setStep} step={step}  />;
         case 2:
-          return <JobIssues  setStep={setStep} step={step} useFormik={useFormik} />;
+          return <JobIssues  setStep={setStep} step={step}  />;
         case 3:
-          return <Contact  setStep={setStep} step={step} useFormik={useFormik} />;
+          return <Contact  setStep={setStep} step={step}  />;
         case 4:
-          return <Verfication  setStep={setStep} step={step} useFormik={useFormik} />;
+          return <Verfication  setStep={setStep} step={step}  />;
          
         default:
           break;
