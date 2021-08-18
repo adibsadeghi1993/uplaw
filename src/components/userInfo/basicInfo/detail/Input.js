da626ddc08ed1fbb3cde3f46476fdb51eb31e0df
adib sadeghi <adibsadeghi7272@gmail.com>
2021-08-18 14:15:58 +0430

    component input with formik was created to handle all inputs

--- src/components/userInfo/basicInfo/detail/Input.js
+++ src/components/userInfo/basicInfo/detail/Input.js
@@ -3,7 +3,15 @@ import React from 'react'
 const Input = ({name,type,placeholder,formik}) => {
     return (
         <>
-            
+           <input
+              name={name}
+              type={type}
+              placeholder={placeholder}
+              {...formik.getFieldProps({name})}
+            />
+             {formik.errors[name] && formik.touched[name] && (
+          <div className="error">{formik.errors[name]}</div>
+        )}  
         </>
     )
 }
