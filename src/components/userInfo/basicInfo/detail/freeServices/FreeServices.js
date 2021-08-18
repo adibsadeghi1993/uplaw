c4f94762e745bd4aa927416c26bc28433aaba62e
adib sadeghi <adibsadeghi7272@gmail.com>
2021-08-18 21:59:12 +0430

    freeServices css was completed

--- src/components/userInfo/basicInfo/detail/freeServices/FreeServices.js
+++ src/components/userInfo/basicInfo/detail/freeServices/FreeServices.js
@@ -1,20 +1,37 @@
-import React from 'react'
-import "./freeservices.css"
-import "../../../../../asstes/signature.svg"
-import "../../../../../asstes/"
-import "../../../../../asstes/registermeeting.svg"
+import React from "react";
+import "./freeservices.css";
+import signatureImage from "../../../../../asstes/signature.svg";
+import adjustImage from "../../../../../asstes/adjust.svg.svg";
+import registermeeting from "../../../../../asstes/registermeeting.svg";
 
 const FreeServices = () => {
-    return (
-        <section className="freeservices">
-            <div className="freeservices_info">
-                <p>با تکمیل ثبت نام از امکانات روبرو <br/>به صورت <strong>رایگان</strong> استفاده کنید.</p>
-                <p>جلوتر از <span>544</span> نفر در تکمیل ثبت نام</p>
-            </div>
-            <div className="freeservices_icons"></div>
-            
-        </section>
-    )
-}
+  return (
+    <section className="freeservices">
+      <div className="freeservices_info">
+        <p className="freeFacility">
+          با تکمیل ثبت نام از امکانات روبرو <br />
+          به صورت <span>رایگان</span> استفاده کنید.
+        </p>
+        <p className="completedRegistration" >
+          جلوتر از <span>544</span> نفر در تکمیل ثبت نام
+        </p>
+      </div>
+      <div className="freeservices_icons">
+        <div className="adjustagreement">
+          <div className="service">  <img src={adjustImage}/></div>
+            <p>تنظیم هوشمند <br/>یک قرارداد</p>
+        </div>
+        <div className="signature ">
+        <div className="service"><img src={signatureImage}/></div>
+        <p>ثبت امضای <br/>دیجیتال</p>
+        </div>
+        <div className="register ">
+        <div className="service"><img src={registermeeting}/></div>
+        <p>ثبت یک <br/>صورت جلسه</p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default FreeServices
+export default FreeServices;
