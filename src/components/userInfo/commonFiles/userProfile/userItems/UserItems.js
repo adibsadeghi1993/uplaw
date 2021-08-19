491f4b81f19dd524a2ff4f20f8beef0ec346ef58
adib sadeghi <adibsadeghi7272@gmail.com>
2021-08-19 18:58:29 +0430

    freeservisec was reresponsived

--- src/components/userInfo/commonFiles/userProfile/userItems/UserItems.js
+++ src/components/userInfo/commonFiles/userProfile/userItems/UserItems.js
@@ -1,55 +1,42 @@
-import React,{useState} from "react";
+import React from "react";
 import "./userItems.css";
-import meetingImage from "../../../../../asstes/meeting.svg"
-import adjustImage from "../../../../../asstes/adjust.svg"
-import adviseImage from "../../../../../asstes/advise.svg"
-import teamImage from "../../../../../asstes/team.svg"
-
+import meetingImage from "../../../../../asstes/meeting.svg";
+import adjustImage from "../../../../../asstes/adjust.svg";
+import adviseImage from "../../../../../asstes/advise.svg";
+import teamImage from "../../../../../asstes/team.svg";
 
 const UserItems = () => {
-  const [width, setWidth]   = useState(window.innerWidth);
-  const updateDimensions = () => {
-      setWidth(window.innerWidth);   
-  }
-  useEffect(() => {
-      window.addEventListener("resize", updateDimensions);
-      return () => window.removeEventListener("resize", updateDimensions);
-  }, []);
-  
   return (
     <div>
-        <section className="userItems">
-      <div className="section_one">
-        <div className="userItems_item">
-          <div className="userItems_item_flex">
+      <section className="userItems">
+        <div className="userItems_item meeting">
+          <div className="userItems_item_container ">
             <img src={meetingImage} alt="slider" />
           </div>
           <p>صورت جلسه</p>
         </div>
-        <div className="userItems_item">
-          <div className="userItems_item_flex adjust">
+        <div className="userItems_item adjust">
+          <div className="userItems_item_container adjust-container ">
             <img src={adjustImage} alt="adjust" />
           </div>
           <p>تنظیم هوشمند</p>
         </div>
-      </div>
-      <div className="section_tow">
-        <div className="userItems_item">
-          <div className="userItems_item_flex">
+
+        <div className="userItems_item consultant">
+          <div className="userItems_item_container">
             <img src={adviseImage} alt="consultant" />
           </div>
           <p>مشاوره</p>
         </div>
-        <div className="userItems_item">
-          <div className="userItems_item_flex teamwork">
+        <div className="userItems_item myteam">
+          <div className="userItems_item_container ">
             <img src={teamImage} alt="teamwork" />
           </div>
           <p>تیم من</p>
         </div>
-      </div>
-    </section>
-    
-    <div className="creditbtn">اعتبار من</div>
+      </section>
+
+      <div className="creditbtn">اعتبار من</div>
       <div className="supportbtn">تیم ها</div>
       <div className="financialbtn">سوابق مالی</div>
     </div>
