2552df53d87d6c278eb66b78863d07f57919801e
adib sadeghi <adibsadeghi7272@gmail.com>
2021-08-19 22:42:23 +0430

    responsive was completed

--- src/components/userInfo/basicInfo/detail/freeServices/FreeServices.js
+++ src/components/userInfo/basicInfo/detail/freeServices/FreeServices.js
@@ -1,45 +1,64 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./freeservices.css";
 import signatureImage from "../../../../../asstes/signature.svg";
 import adjustImage from "../../../../../asstes/adjust.svg.svg";
 import registermeeting from "../../../../../asstes/registermeeting.svg";
 
 const FreeServices = () => {
-    const [width, setWidth]   = useState(window.innerWidth);
-const updateDimensions = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const updateDimensions = () => {
     setWidth(window.innerWidth);
-    
-}
-useEffect(() => {
+  };
+  useEffect(() => {
     window.addEventListener("resize", updateDimensions);
     return () => window.removeEventListener("resize", updateDimensions);
-}, []);
+  }, []);
   return (
     <section className="freeservices">
       <div className="freeservices_info">
-        {width <890 ?<p className="freeFacility">
-           با تکمیل فرم ثبت نام از امکانات زیر<br />
-          به صورت <span>رایگان</span> استفاده کنید.
-        </p>:<p className="freeFacility">
-          با تکمیل ثبت نام از امکانات روبرو <br />
-          به صورت <span>رایگان</span> استفاده کنید.
-        </p>}
-        <p className="completedRegistration" >
+        {width < 890 ? (
+          <p className="freeFacility">
+            با تکمیل فرم ثبت نام از امکانات زیر
+            <br />
+            به صورت <span>رایگان</span> استفاده کنید.
+          </p>
+        ) : (
+          <p className="freeFacility">
+            با تکمیل ثبت نام از امکانات روبرو <br />
+            به صورت <span>رایگان</span> استفاده کنید.
+          </p>
+        )}
+        <p className="completedRegistration">
           جلوتر از <span>544</span> نفر در تکمیل ثبت نام
         </p>
       </div>
       <div className="freeservices_icons">
         <div className="adjustagreement">
-          <div className="service">  <img src={adjustImage}/></div>
-            <p>تنظیم هوشمند <br/>یک قرارداد</p>
+          <div className="service">
+            <img src={adjustImage} />
+          </div>
+          <p>
+            تنظیم هوشمند <br />
+            یک قرارداد
+          </p>
         </div>
         <div className="signature ">
-        <div className="service"><img src={signatureImage}/></div>
-        <p>ثبت امضای <br/>دیجیتال</p>
+          <div className="service">
+            <img src={signatureImage} />
+          </div>
+          <p>
+            ثبت امضای <br />
+            دیجیتال
+          </p>
         </div>
         <div className="register ">
-        <div className="service"><img src={registermeeting}/></div>
-        <p>ثبت یک <br/>صورت جلسه</p>
+          <div className="service">
+            <img src={registermeeting} />
+          </div>
+          <p>
+            ثبت یک <br />
+            صورت جلسه
+          </p>
         </div>
       </div>
     </section>
