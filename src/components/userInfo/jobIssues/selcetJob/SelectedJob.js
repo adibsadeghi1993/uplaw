1f2a85d0c1f76a3108de2131211f839fc00cf68d
adib sadeghi <adibsadeghi7272@gmail.com>
2021-08-20 16:29:32 +0430

    useEffect was writed to handle background when user click on selectedJob

--- src/components/userInfo/jobIssues/selcetJob/SelectedJob.js
+++ src/components/userInfo/jobIssues/selcetJob/SelectedJob.js
@@ -1,17 +1,28 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-const SelectedJob = ({job,setJob}) => {
-    const [jobOptions, setJobOptions] = useState([
-        { jobTitle: "استارتاپ/شرکت دارم", id: 1, background: "white" },
-        { jobTitle: "فریلنسر هستم", id: 2, background: "white" },
-        { jobTitle: "کارمند هستم", id: 3, background: "white" },
-      ]);
+const SelectedJob = ({ job, setJob }) => {
+  const [jobOptions, setJobOptions] = useState([
+    { jobTitle: "استارتاپ/شرکت دارم", id: 1, background: "white" },
+    { jobTitle: "فریلنسر هستم", id: 2, background: "white" },
+    { jobTitle: "کارمند هستم", id: 3, background: "white" },
+  ]);
 
-      const jobHandler = (item) => {
-        usersJob(item, job, setJob);
-      };
-    return (
-        <section className="selectJob_info">
+  const jobHandler = (item) => {
+    usersJob(item, job, setJob);
+  };
+
+  useEffect(() => {
+    const newJobOptions = jobOptions.map((item) => {
+      if (job.find((job) => job.jobTitle === item.jobTitle)) {
+        return { ...item, background: "blue" };
+      } else {
+        return item;
+      }
+    });
+    setJobOptions(newJobOptions);
+  }, [job]);
+  return (
+    <section className="selectJob_info">
       <p>شما؟</p>
       <div className="selectJob_info_main">
         {jobOptions.map((item) => {
@@ -26,7 +37,7 @@ const SelectedJob = ({job,setJob}) => {
         })}
       </div>
     </section>
-    )
-}
+  );
+};
 
-export default SelectedJob
+export default SelectedJob;
