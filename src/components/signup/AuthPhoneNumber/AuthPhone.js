3b0e549acc69c743777db80a2a47ad45bac8675f
adib sadeghi <adibsadeghi7272@gmail.com>
2021-08-22 20:56:46 +0430

    image added to assets folder

--- src/components/signup/AuthPhoneNumber/AuthPhone.js
+++ src/components/signup/AuthPhoneNumber/AuthPhone.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import circleArrowImage from "../../../asstes/circlearrow.svg"
 
 const AuthPhone = () => {
     return (
-        <div>
+        <section className="authphone_section">
+            <div>
+                <input type="text" placeholder="شماره تلفن خود را وارد کنید"/>
+                <button><img src={circleArrowImage} /></button>
+            </div>
             
-        </div>
+        </section>
     )
 }
 
--- src/components/signup/Authgoogle/AuthGoogle.js
+++ src/components/signup/Authgoogle/AuthGoogle.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import "./AuthGoogle.css"
+import googleImage from "../../../asstes/google.svg"
+import arrowImage from "../../../asstes/arrow.svg"
 
 const AuthGoogle = () => {
     return (
         <section className="google_section">
             <div>
-                <button></button>
+                <button>از گوگل برای ثبت نام/ورود استفاده کنید<img src={googleImage}/></button>
+            </div>
+            <div>
+                <input type="checkbox"/>
+                <label>با قوانین و مقررات موافقم</label>
+            </div>
+            <div>
+                <button>وارد شوید <img src={arrowImage}/></button>
             </div>
           
         </section>
