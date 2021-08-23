92a0bc1d4721590bc9eb41db8f4e7ae47cc3ba92
adib sadeghi <adibsadeghi7272@gmail.com>
2021-08-23 13:42:00 +0430

    alt attribute was added to the images

--- src/components/signup/AuthPhoneNumber/AuthPhone.js
+++ src/components/signup/AuthPhoneNumber/AuthPhone.js
@@ -10,15 +10,15 @@ const AuthPhone = () => {
         <>
             <div className="authphone_phonenumber">
                 <input type="text" placeholder="شماره تلفن خود را وارد کنید"/>
-                <button><img src={circleArrowImage} /></button>
-                <img className="phoneImage" src={phoneImage} />
+                <button><img src={circleArrowImage} alt="circle arrow image" /></button>
+                <img className="phoneImage" src={phoneImage} alt="phone Image" />
             </div>
             <div className="authphone_sendcode">
                 <p>ارسال مجدد کد تا 05:00 دقیقه</p>
             </div>
             <div className="authphone_code">
                 <input type="text" placeholder="کد ارسال شده را وارد کنید"/>
-                <img src={lockImage}/>
+                <img src={lockImage} alt="lockImage"/>
                 
             </div>
             
--- src/components/signup/Authgoogle/AuthGoogle.js
+++ src/components/signup/Authgoogle/AuthGoogle.js
@@ -1,24 +1,48 @@
-import React from 'react'
-import "./AuthGoogle.css"
-import googleImage from "../../../asstes/google (2).svg"
-import arrowImage from "../../../asstes/arrow2.svg"
+import React from "react";
+import "./AuthGoogle.css";
+import { GoogleLogin } from 'react-google-login'
+import googleImage from "../../../asstes/google (2).svg";
+import arrowImage from "../../../asstes/arrow2.svg";
 
 const AuthGoogle = () => {
-    return (
-        <>
-            <div className="authsection_google">
-                <button><img src={googleImage}/><span>از گوگل برای ثبت نام/ ورود استفاده کنید</span></button>
-            </div>
-            <div className="condition">
-                <input type="checkbox" id="checkboxCondition"/>
-                <label htmlFor="checkboxCondition">با قوانین و مقررات موافقم .</label>
-            </div>
-            <div className="confirm_signup">
-                <button><span>وارد شوید</span> <img src={arrowImage}/></button>
-            </div>
-          
-        </>
-    )
-}
 
-export default AuthGoogle
+    const responseSuccessGoogle=(response)=>{
+        console.log(response)
+    }
+    const responseErrorGoogle=(response)=>{
+        console.log(response)
+    }
+  return (
+    <>
+      <div className="authsection_google">
+        <GoogleLogin
+          clientId="857095448578-hb4jvirn25baa763ue2d9s13ri5o7vls.apps.googleusercontent.com"
+          render={(renderProps) => (
+            <button
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+            >
+              <img src={googleImage} alt="google image" />
+              <span>از گوگل برای ثبت نام/ ورود استفاده کنید</span>
+            </button>
+          )}
+          buttonText="Login"
+          onSuccess={responseSuccessGoogle}
+          onFailure={responseErrorGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      </div>
+      <div className="condition">
+        <input type="checkbox" id="checkboxCondition" />
+        <label htmlFor="checkboxCondition">با قوانین و مقررات موافقم .</label>
+      </div>
+      <div className="confirm_signup">
+        <button>
+          <span>وارد شوید</span> <img src={arrowImage} alt="arrow image" />
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default AuthGoogle;
