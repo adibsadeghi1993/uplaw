0fa8430f6f6e0a5269013824f37837a16f43c96a
adib sadeghi <adibsadeghi7272@gmail.com>
2021-08-26 14:38:43 +0430

    input type file was added to upload nationalcode

--- src/components/userInfo/userVerification/verificationDetailes/VerificationDetailes.js
+++ src/components/userInfo/userVerification/verificationDetailes/VerificationDetailes.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import styles from "./verificationDetailes.module.css"
+import React from "react";
+import UserStep from "../../commonFiles/userStep/UserStep";
+import styles from "./verificationDetailes.module.css";
 
 const VerificationDetailes = () => {
-    return (
-        <section className={styles.verification_detaile}>
-            
-        </section>
-    )
-}
+  return (
+    <section className={styles.verification_detaile}>
+      <UserStep />
+      <section className={styles.nationalcode_desc}>
+        <div className={styles.description}>
+          <p>
+            برای استفاده از تمامی امکانات داشبورد آپلا، لطفا عکس روی کارت ملی
+            خود را آپلوئ کنید
+          </p>
+        </div>
+        <div className={styles.nationalcode}>
+          <input
+            type="file"
+            accept="image/*"
+            id="image"
+            name="image-upload"
+            // onChange={imageHandler}
+          />
+        </div>
+      </section>
+    </section>
+  );
+};
 
-export default VerificationDetailes
+export default VerificationDetailes;
