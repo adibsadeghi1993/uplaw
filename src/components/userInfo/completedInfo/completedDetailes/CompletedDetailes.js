11d2e46bd29cc098f74893b566e9ff844e7027b4
adib sadeghi <adibsadeghi7272@gmail.com>
2021-08-28 22:34:39 +0430

    icons was added to the assets

--- src/components/userInfo/completedInfo/completedDetailes/CompletedDetailes.js
+++ src/components/userInfo/completedInfo/completedDetailes/CompletedDetailes.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import styles from "./completedDetailes.module.css"
+import completedImage from  "../../../../asstes/completedpic.svg"
 
 const CompletedDetailes = () => {
     return (
         <main className={styles.completed_detailes} >
-            completed_detailes
+            <section className={styles.completede_desc}>
+                <p>پروفایل شما کامل و تایید شد! از بسته رایگانتون امکانات زیر باقیه!</p>
+                <img src={completedImage}  />
+            </section>
         </main >
     )
 }
