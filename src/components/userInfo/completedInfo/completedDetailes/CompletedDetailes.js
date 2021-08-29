13c8bdbc3eea70db8d229783885486da5b4cd67e
adib sadeghi <adibsadeghi7272@gmail.com>
2021-08-29 19:00:27 +0430

    secton three of completedDetiles was finished

--- src/components/userInfo/completedInfo/completedDetailes/CompletedDetailes.js
+++ src/components/userInfo/completedInfo/completedDetailes/CompletedDetailes.js
@@ -8,6 +8,7 @@ import contractImage from "../../../../asstes/blueadjustcontract.svg"
 import signatureImage from "../../../../asstes/blue signature.svg"
 import adjustMeetingImage from "../../../../asstes/blueadjustmeeting.svg"
 import blueArrowImage from "../../../../asstes/blue arrow.svg"
+import { Docs } from "./Docs/Docs";
 
 const CompletedDetailes = () => {
   const percentage = 66;
@@ -27,12 +28,20 @@ const CompletedDetailes = () => {
              <p style={{color:"black",fontSize: 14,fontWeight:900}}>روز</p>
             </div>
           </CircularProgressbarWithChildren>
+          <div className={styles.epmty}></div>
         </div>
         
         <div className={styles.contract}><img src={contractImage} className={styles.image} /><div className={styles.descdiv}><span>یک</span>تنظیم هوشمند قرارداد</div></div>
         <div className={styles.meeting}><img src={adjustMeetingImage} className={styles.image}/><div  className={styles.descdiv}><span>یک</span>تنظیم صورت جلسه</div></div>
         <div className={styles.signature}><img src={signatureImage} className={styles.image}/><div  className={styles.descdiv}>ثبت امضای دیجیتال</div></div>
         <button className={styles.begainButton}><img src={blueArrowImage}/>شروع میکنم</button>
+      </section>
+      <div className={styles.pridication}><p>اسناد پرطرفدار در آپلا</p></div>
+      <section className={styles.completed_detailes_sectionThree}>
+     {Docs.map((item)=>{
+       return  <div key={item.id} className={styles[item.className]}><div className={styles.eachDocs}><p>{item.title}</p></div><button className={styles.adjustbtn}>تنظیم هوشمند <img src={blueArrowImage}/></button></div>
+        
+     })}
       </section>
     </main>
   );
