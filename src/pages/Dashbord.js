4b6ad1ef0b76fa77e0fb97f346815d8a7047802f
adib sadeghi <adibsadeghi7272@gmail.com>
2021-09-01 12:27:17 +0430

    UserTeams component was created

--- src/pages/Dashbord.js
+++ src/pages/Dashbord.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ContextInfo from '../components/userInfo/contextInfo/ContextInfo';
 import DashbordComponents from '../components/userInfo/DashbordComponents';
+import UserTeams from '../components/userInfo/userTeams/UserTeams';
 
 const Dashbord = () => {
 
     return (
         <ContextInfo>
-         <DashbordComponents/>
+         {/* <DashbordComponents/> */}
+         <UserTeams/>
         </ContextInfo>
     )
 }
