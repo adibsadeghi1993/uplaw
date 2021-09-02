1bdd87320b2a5249ddd7cdeef56cdb2bae4deec5
adib sadeghi <adibsadeghi7272@gmail.com>
2021-09-02 20:55:17 +0430

    createTeam was responsived for mobile

--- src/components/userInfo/createTeam/meeting/Meeting.js
+++ src/components/userInfo/createTeam/meeting/Meeting.js
@@ -5,7 +5,7 @@ import blueArrowImage from "../../../../asstes/blue arrow.svg";
 
 const Meeting = () => {
   const meetings = [
-    { meetingName: "صورت جلسه تغغیرات", date: "28/5/1400", name: "آپلا" },
+    { meetingName: "صورت جلسه تغییرات", date: "28/5/1400", name: "آپلا" },
     { meetingName: "جلسه طراحی UI", date: "28/5/1400", name: "آقای خانی" },
     {
       meetingName: "جلسه انعقاد قرارداد",
@@ -14,7 +14,7 @@ const Meeting = () => {
     },
   ];
   return (
-    <section>
+    <section className={styles.main_section_metting}>
       <div className={styles.meeting}>
         <img src={meetingImage} />
         <p>آخرین صورت جلسات</p>
@@ -22,15 +22,28 @@ const Meeting = () => {
       <section className={styles.meeting_section}>
         {meetings.map((item) => {
           return (
-            <article className={styles.meeting_changes}>
-              <p>{item.meetingName}</p>
-              <p>{item.date}</p>
-              <p>{item.name}</p>
-              <div><span>مشاهده</span><img src={blueArrowImage}/></div>
+            <article className={styles.eachmeeting}>
+              <p className={styles.meetingName}>{item.meetingName}</p>
+              <p className={styles.date}>{item.date}</p>
+              <p className={styles.username}>{item.name}</p>
+              <div className={styles.observe_meeting}><span>مشاهده</span><img src={blueArrowImage}/></div>
             </article>
           );
         })}
       </section>
+      <section className={styles.dropdown_section}>
+          {meetings.map((item)=>{
+              return <div className={styles.eachselect}>
+                  <select className={styles.select}>
+                  <option selected>{item.meetingName}</option>
+                   <option vlaue={item.date}>{item.date}</option>
+                   <option vlaue={item.name}>{item.name}</option>
+              </select>
+              </div>
+          })}
+
+      </section>
+      <div className={styles.adjust_newmeeting}><span>تنظیم صورت جلسه جدید</span><img src={blueArrowImage}/></div>
     </section>
   );
 };
--- src/components/userInfo/userTeams/userTeamsDocs/variousDocs/VariousDocs.js
+++ src/components/userInfo/userTeams/userTeamsDocs/variousDocs/VariousDocs.js
@@ -1,35 +1,51 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import styles from "./variousDocs.module.css";
-import blueArrow from "../../../../../asstes/blue arrow.svg"
+import blueArrow from "../../../../../asstes/blue arrow.svg";
 
 const VariousDocs = () => {
-    const [statusDoc, setstatusDocs] = useState(true)
+  const [statusDoc, setstatusDocs] = useState(true);
 
   const docs = [
     { docName: "تفاهمنامه سهامداران", id: 1 },
     { docName: "قرارداد استخدام برنامه نویس", id: 2 },
     { docName: "اساسنامه شرکت <span>آپلا</span>", id: 3 },
   ];
-  const businessHandler=()=>{
-      setstatusDocs(false)
-  }
-  const legalHandler=()=>{
-    setstatusDocs(true)
-}
+  const businessHandler = () => {
+    setstatusDocs(false);
+  };
+  const legalHandler = () => {
+    setstatusDocs(true);
+  };
   return (
     <>
       <div className={styles.doc}>
-        <p onClick={legalHandler}  className={`${styles.legal_docs} ${statusDoc ? styles.active :""}` }>اسناد حقوقی</p> <p onClick={businessHandler} className={`${styles.business_docs} ${!statusDoc ? styles.active :""}`}>اسناد کسب و کاری</p>
+        <p
+          onClick={legalHandler}
+          className={`${styles.legal_docs} ${statusDoc ? styles.active : ""}`}
+        >
+          اسناد حقوقی
+        </p>
+        <p
+          onClick={businessHandler}
+          className={`${styles.business_docs} ${
+            !statusDoc ? styles.active : ""
+          }`}
+        >
+          اسناد کسب و کاری
+        </p>
       </div>
       <div className={styles.alldocs}>
         {docs.map((item) => {
           return (
             <div className={`${styles.eachDoc} ${styles[`doc${item.id}`]}`}>
-                <p dangerouslySetInnerHTML={{
-                    __html: item.docName
-                }}>
-                </p>
-              <button className={styles.docbtn}><p className={styles.observe}>مشاهده</p> <img src={blueArrow}/></button>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: item.docName,
+                }}
+              ></p>
+              <button className={styles.docbtn}>
+                <p className={styles.observe}>مشاهده</p> <img src={blueArrow} />
+              </button>
             </div>
           );
         })}
