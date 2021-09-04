dd8a4f0ac8cd01d12cdb7ffb82acc2355a6e1a41
adib sadeghi <adibsadeghi7272@gmail.com>
2021-09-04 16:22:16 +0430

    add route to dashbord and home

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Dashbord from './pages/Dashbord';
 import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
 import SignUp from "./pages/SignUp";
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
     
       <Switch>
-        <Route path="/auth/google" exact component={SignUp}/>
-        <Route path="/dashbord" exact component={Dashbord}/>
+        <Route path="/auth/google" component={SignUp}/>
+        <Route path="/dashbord" component={Dashbord}/>
+        <Route path="/" exact component={Home}/>
       </Switch>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
         <div>
-            home
+          <Link to="/dashbord">  <h3>go to the dashbord</h3></Link>
+          <Link to="/auth/google">  <h3>go to the sginup/login</h3></Link>
         </div>
     )
 }
