2690c3dfe03515ff0963095c82fa5449ddf3d3dd
adib sadeghi <adibsadeghi7272@gmail.com>
2021-09-07 17:28:25 +0430

    phoneNumber actions and constants was created

--- src/redux/Actions/SignUpAction.js
+++ src/redux/Actions/SignUpAction.js
@@ -1,6 +1,6 @@
 
-import { formatError, saveTokenInLocalStorage, signUpGoogle } from "../../services/userInfoServices";
-import { CONFIRMED_AUTH_GOOGLE, FAILED_AUTH_GOOGLE } from "../Constants";
+import { formatError, saveTokenInLocalStorage, signUpGoogle, signUpValidateCode } from "../../services/userInfoServices";
+import { CONFIRMED_AUTH_GOOGLE, CONFIRMED_AUTH_PHONENUMBER, FAILED_AUTH_GOOGLE, FAILED_AUTH_PHONENUMBER } from "../Constants";
 
 export const signUpGooleAction = (data) => {
   
@@ -20,7 +20,6 @@ export const signUpGooleAction = (data) => {
 };
 
 
-
 const confrimSignUpGoogleAction = (data) => {
   return {
     type: CONFIRMED_AUTH_GOOGLE,
@@ -34,3 +33,37 @@ const failedSignUpGoogleAction = (data) => {
       payload: data,
     };
   };
+
+
+
+export const signUpPhoneNumberAction = (data) => {
+  
+  return async(dispatch) => {
+    try {
+        const response=await signUpValidateCode(data)
+        console.log(response.data)
+       
+        dispatch(confrimSignUpPhoneNumberAction(response.data))
+        saveTokenInLocalStorage(response.data.data.token)
+        
+    } catch (error) {
+        const errorMessage=formatError(error.response)
+        dispatch(failedSignUpPhoneNumberAction(errorMessage))
+    }
+  };
+};
+
+
+  const confrimSignUpPhoneNumberAction = (data) => {
+    return {
+      type: CONFIRMED_AUTH_PHONENUMBER,
+      payload: data,
+    };
+  };
+  
+  const failedSignUpPhoneNumberAction = (data) => {
+      return {
+        type: FAILED_AUTH_PHONENUMBER,
+        payload: data,
+      };
+    };
