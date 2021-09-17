1fa18ef9a58ee9257dc73c0cd1077408bb8923bd
adib sadeghi <adibsadeghi7272@gmail.com>
2021-09-17 16:48:08 +0430

    ContractInfo component was created and svg added

---
+++ src/components/userInfo/adjustContract/contractInfo/ContractInfo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./contractInfo.module.css"
+
+const ContractInfo = () => {
+    return (
+        <section className={styles.main_contract_info}>
+            
+        </section>
+    )
+}
+
+export default ContractInfo
