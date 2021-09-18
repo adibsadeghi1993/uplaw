c5a30a0f84176c76823ce16dd86c4a5b740dd0fa
adib sadeghi <adibsadeghi7272@gmail.com>
2021-09-18 17:37:05 +0430

    images was added to ContractInfo component

--- src/components/userInfo/adjustContract/contractInfo/ContractInfo.js
+++ src/components/userInfo/adjustContract/contractInfo/ContractInfo.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import styles from "./contractInfo.module.css"
+import addImage from "../../../../asstes/add.svg"
+import minesImage from "../../../../asstes/mines.svg"
+import confirmImage from "../../../../asstes/confirm.svg"
 
 const ContractInfo = () => {
     return (
         <section className={styles.main_contract_info}>
+            <div><img src={addImage} alt="تایید"/></div>
+            <div><img src={minesImage} alt="تایید"/></div>
+            <div><img src={confirmImage} alt="تایید"/></div>
+
             
         </section>
     )
