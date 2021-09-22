ad31f83139c16fc569f46cc3b0bf576a42c206c6
adib sadeghi <adibsadeghi7272@gmail.com>
2021-09-22 22:57:19 +0330

    PdfDoc and CustomerSingture components was created

---
+++ src/components/contract/SigntureContract/CustomerSignture/CustomerSignture.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./CustomerSignture.module.css"
+
+const CustomerSignture = () => {
+    return (
+        <main >
+            
+        </main>
+    )
+}
+
+export default CustomerSignture
---
+++ src/components/contract/commonContractFiles/PdfDoc/PdfDoc.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./PdfDoc.module.css"
+
+const PdfDoc = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PdfDoc
