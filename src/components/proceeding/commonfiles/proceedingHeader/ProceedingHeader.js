a68b099c1e30ffec57c0fe63b4d65581922b1aa7
adib sadeghi <adibsadeghi7272@gmail.com>
2021-09-25 13:30:05 +0330

    proceedingHeader component was split because is reused compoent

---
+++ src/components/proceeding/commonfiles/proceedingHeader/ProceedingHeader.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from "./proceedingHeader.module.css"
+import fingerImage from "../../../../asstes/finger.png"
+
+const ProceedingHeader = () => {
+    return (
+        <div className={styles.proceeding_create_team}>
+        <h1>
+          این صفحه برای ساختن هر نوع صورت جلسه ایه! تو میتونی برای هر صورت
+          جلسه اعضای مورد نظرت رو دعوت کنی و صورت جلسه رو درست کنی.
+        </h1>
+        <img src={fingerImage} alt="تنظیم صورت جلسه" />
+      </div>
+    )
+}
+
+export default ProceedingHeader
--- src/components/proceeding/proceedingDetailes/ProceedingDetailes.js
+++ src/components/proceeding/proceedingDetailes/ProceedingDetailes.js
@@ -1,8 +1,8 @@
 import React, { useState, useContext } from "react";
-import fingerImage from "../../../asstes/finger.png";
 import blueArrowImage from "../../../asstes/blue arrow.svg";
 import { useSelector } from 'react-redux'
 import styles from "./procedingDetailes.module.css";
+import ProceedingHeader from "../commonfiles/proceedingHeader/ProceedingHeader";
 
 
 const ProceedingDetailes = () => {
@@ -19,14 +19,8 @@ const ProceedingDetailes = () => {
   return (
     <main className={styles.proceeding_main}>
       <section>
-        <div className={styles.proceeding_create_team}>
-          <h1>
-            این صفحه برای ساختن هر نوع صورت جلسه ایه! تو میتونی برای هر صورت
-            جلسه اعضای مورد نظرت رو دعوت کنی و صورت جلسه رو درست کنی.
-          </h1>
-          <img src={fingerImage} alt="تنظیم صورت جلسه" />
-        </div>
-       
+      
+         <ProceedingHeader/>
         <p className={styles.guide_paragraf}>
           تیم مورد نظر خود را برای ساخت صورت جلسه انتخاب کنید.
         </p>
