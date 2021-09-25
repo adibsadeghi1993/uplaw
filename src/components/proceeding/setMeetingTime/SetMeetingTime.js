fec83c10dae77763c563b6dfb87f6a3c11775292
adib sadeghi <adibsadeghi7272@gmail.com>
2021-09-25 17:49:10 +0330

    myTeam was added to the SetMeeting component

--- src/components/proceeding/setMeetingTime/SetMeetingTime.js
+++ src/components/proceeding/setMeetingTime/SetMeetingTime.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import styles from "./setMeetingTime.module.css"
+import React from "react";
+import { useSelector } from "react-redux";
+import UserProfile from "../../userInfo/commonFiles/userProfile/UserProfile";
+import styles from "./setMeetingTime.module.css";
 
 const SetMeetingTime = () => {
-    return (
+  const { myTeam } = useSelector((state) => state.team);
+  return (
+    <main className={styles.main_proceeding}>
+      <section className={styles.profilesection}>
+        <UserProfile opacity="opacity" team={myTeam} />
+      </section>
+      <section className={styles.proceeding_info}>
+        <div className={styles.proceeding_detail}>
+          <h1>صورت جلسه/ </h1>
+        </div>
+      </section>
+    </main>
+  );
+};
 
-
-           <main className={styles.main_proceeding}>
-        <section className={styles.profilesection}>
-         <UserProfile opacity="opacity" team={ myTeam}/>
-        </section>
-        <section className={styles.proceeding_info}>
-         <div className={styles.proceeding_detail}>
-           <h1>صورت جلسه/ </h1>
-                <CreateMeetingDetailes/>
-         </div>
-        </section>
-      </main>
-      
-    )
-}
-
-export default SetMeetingTime
+export default SetMeetingTime;
