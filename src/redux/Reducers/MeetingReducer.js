00b73a64f5ce070c6634d456db094c5ab0ecc996
adib sadeghi <adibsadeghi7272@gmail.com>
2021-09-26 22:02:39 +0330

    action and reducer was created form meeting

---
+++ src/redux/Reducers/MeetingReducer.js
@@ -0,0 +1,9 @@
+const initialState={
+    setTime:{
+        date:"",
+        time:""
+    }
+}
+export const meetingReducer=(state=initialState,action)=>{
+    return state
+}
--- src/redux/Reducers/RootReducer.js
+++ src/redux/Reducers/RootReducer.js
@@ -1,9 +1,14 @@
 import { combineReducers } from "redux";
 import { contractReducer } from "./ContractReducer";
 import { teamReducer } from "./TeamReducer";
+import { meetingReducer } from "./MeetingReducer";
 
 import { userInfoReducer } from "./UserInfoReducer";
 
-
-
-export const rootReducer=combineReducers({userInfo:userInfoReducer,contract:contractReducer,team:teamReducer})
+export const rootReducer = combineReducers({
+  userInfo: userInfoReducer,
+  contract: contractReducer,
+  team: teamReducer,
+  meeting:meetingReducer
+ 
+});
