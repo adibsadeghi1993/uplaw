2006aeb8b8fc4768e41cf518a0f93c47248c8aa6
adib sadeghi <adibsadeghi7272@gmail.com>
2021-09-26 13:36:24 +0330

    main layout of signture meeting was created

---
+++ src/components/proceeding/signtureMeeting/SigntureMeeting.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from "./signtureMeeting.module.css"
+import { useSelector } from "react-redux";
+import UserProfile from '../../userInfo/commonFiles/userProfile/UserProfile';
+
+const SigntureMeeting = () => {
+    const { myTeam } = useSelector((state) => state.team);
+    return (
+        <main className={styles.main_signture_meeting}>
+      <section className={styles.profilesection}>
+        <UserProfile opacity="opacity" team={myTeam} />
+      </section>
+      <section className={styles.signture_meeting_info}>
+        <div className={styles.signture_meeting_detail}>
+          <h1 className={styles.main_title}>صورت جلسه/ </h1>
+          
+          
+        </div>
+      </section>
+    </main>
+    )
+}
+
+export default SigntureMeeting
---
+++ src/components/proceeding/signtureMeeting/signtureMeetingDetailes/SigntureMeetingDetailes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SigntureMeetingDetailes = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SigntureMeetingDetailes
