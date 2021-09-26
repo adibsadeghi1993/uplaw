f5c6ec4ae6f3d98286ab61e2b49cbb595b14222c
adib sadeghi <adibsadeghi7272@gmail.com>
2021-09-26 13:05:38 +0330

    css was added to TimePicker component

--- src/components/proceeding/setMeetingTime/setMeetingTimeDeatailes/timePicker/TimePicker.js
+++ src/components/proceeding/setMeetingTime/setMeetingTimeDeatailes/timePicker/TimePicker.js
@@ -1,19 +1,29 @@
-import React,{useState} from 'react'
-import styles from  "./timePicker.module.css"
-import 'antd/dist/antd.css';
-import { TimePicker } from 'antd';
+import React, { useState } from "react";
+import "antd/dist/antd.css";
+import moment from "moment";
+import { TimePicker } from "antd";
+import styles from "./timePicker.module.css"
 
 const TimePickerComponent = () => {
-    const [value, setValue] = useState(null);
+  const [value, setValue] = useState(null);
+  const format = "HH:mm";
+  const onChange = (time) => {
+    setValue(time);
+  };
+  return (
+    <>
+      <TimePicker
+        showNow={false}
+        minuteStep={15}
+        defaultValue={moment("12:08", format)}
+        format={format}
+        style={{ width: "100%", height: "49px", borderRadius: "6px" }}
+        placeholder="ساعت تشکیل جلسه را انتخاب کنید."
+        value={value}
+        onChange={onChange}
+      />
+    </>
+  );
+};
 
-    const onChange = time => {
-      setValue(time);
-    };
-    return (
-        <>
-            <TimePicker style={{width:"100%"}} placeholder="plese enter your time" value={value} onChange={onChange} />
-        </>
-    )
-}
-
-export default TimePickerComponent
+export default TimePickerComponent;
