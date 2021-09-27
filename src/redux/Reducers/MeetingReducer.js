0902909c77102af0a636ca639f03bdff157e7175
adib sadeghi <adibsadeghi7272@gmail.com>
2021-09-27 14:52:27 +0330

    reducer for setMeeting was created

--- src/redux/Reducers/MeetingReducer.js
+++ src/redux/Reducers/MeetingReducer.js
@@ -1,9 +1,35 @@
+import { CHOOSED_TEAM_FORM_MEETING, SET_MEETING_DATE, SET_MEETING_TIME } from "../Constants"
+
 const initialState={
-    setTime:{
+    setMeeting:{
         date:"",
-        time:""
+        time:"",
+        teamForMeeting:""
     }
 }
-export const meetingReducer=(state=initialState,action)=>{
-    return state
+export const meetingReducer=(state=initialState,{type,payload})=>{
+    
+    switch (type) {
+        case SET_MEETING_TIME : {
+            let oldState={...state}
+            oldState.setMeeting={...oldState.setMeeting,time:payload}
+            return {...oldState}
+        }
+      
+        case SET_MEETING_DATE : {
+            let oldState={...state}
+            oldState.setMeeting={...oldState.setMeeting,date:payload}
+            return {...oldState}
+        }
+       
+        case CHOOSED_TEAM_FORM_MEETING :{
+            let oldState={...state}
+            oldState.setMeeting={...oldState.setMeeting,teamForMeeting:payload}
+            return {...oldState}
+        } 
+           
+    
+        default:
+            return state
+    }
 }
