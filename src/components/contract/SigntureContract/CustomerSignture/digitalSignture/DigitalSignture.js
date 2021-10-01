fc71f85c8db00624f65010fe4d0bf6704bc00b9d
adib sadeghi <adibsadeghi7272@gmail.com>
2021-10-01 13:42:02 +0330

    DigitalSignture Component was responsive

--- src/components/contract/SigntureContract/CustomerSignture/digitalSignture/DigitalSignture.js
+++ src/components/contract/SigntureContract/CustomerSignture/digitalSignture/DigitalSignture.js
@@ -1,52 +1,48 @@
-import React, { useState, useRef,useEffect } from "react";
-import SignatureCanvas from "react-signature-canvas";
+import React, { useState, useRef, useEffect } from "react";
+import { signtureFunction } from "./signtureFunction";
 import blueArrowImage from "../../../../../asstes/blue arrow.svg";
-import styles from "./digitalSignture.module.css"
-
+import styles from "./digitalSignture.module.css";
 
 
 const DigitalSignture = () => {
-    const [width, setWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [signture, setSignture] = useState(null);
 
-    const updateDimensions = () => {
-      setWidth(window.innerWidth);
-    };
-    useEffect(() => {
-      window.addEventListener("resize", updateDimensions);
-      return () => window.removeEventListener("resize", updateDimensions);
-    }, []);
-    const [imageURL, setImageURL] = useState(null); 
-    const save = () =>
-    setImageURL(sigCanvas.current.toDataURL("image/png"));
-    const sigCanvas = useRef();
-    const clear = () => sigCanvas.current.clear();
-    return (
-        <div>
-            <div className={styles.signture_container}  >
-            <SignatureCanvas
-              ref={sigCanvas}
-              penColor="green"
-              
-              canvasProps={{
-               height:150, width:200,      className: styles.canvas
-              }}
-        
-            />
-            </div>
-             <div className={styles.flex_btns}>
-             <button className={styles.clearButton} onClick={clear}>
-                    <span>پاک کردن امضا</span>
-                    <img src={blueArrowImage} alt="تایید" />
-                  </button>
-                  <button className={styles.saveButton} onClick={save}>
-                    <span>ثبت امضا</span>
-                    <img src={blueArrowImage} alt="تایید" />
-                  </button>
-                 
-                </div>
-          
-        </div>
-    )
-}
+  const updateDimensions = () => {
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+  const [imageURL, setImageURL] = useState(null);
+  const save = () => setImageURL(sigCanvas.current.toDataURL("image/png"));
+  const sigCanvas = useRef();
+  const clear = () => sigCanvas.current.clear();
 
-export default DigitalSignture
+  useEffect(() => {
+    const returendSignture = signtureFunction(width, sigCanvas, styles);
+    console.log(returendSignture);
+    setSignture(returendSignture);
+  }, [width]);
+
+  return (
+    <div>
+      <div className={styles.signture_container} >
+     {signture}
+      </div>
+      <div className={styles.flex_btns}>
+        <button className={styles.clearButton} onClick={clear}>
+          <span>پاک کردن امضا</span>
+          <img src={blueArrowImage} alt="تایید" />
+        </button>
+        <button className={styles.saveButton} onClick={save}>
+          <span>ثبت امضا</span>
+          <img src={blueArrowImage} alt="تایید" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DigitalSignture;
