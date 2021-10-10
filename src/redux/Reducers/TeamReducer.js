f05d8aaa08ab5817faebb99ad7932331c65520f4
adib sadeghi <adibsadeghi7272@gmail.com>
2021-10-10 14:49:23 +0330

    userImage was added to assets

--- src/redux/Reducers/TeamReducer.js
+++ src/redux/Reducers/TeamReducer.js
@@ -1,6 +1,10 @@
 
 import uplawImage from "../../asstes/uplaw.png"
 import { SELECTED_TEAM } from "../constants/TeamConstants"
+import user1 from "../../asstes/user1.svg"
+import user2 from "../../asstes/user2.svg"
+import user3 from "../../asstes/user3.svg"
+import user4 from "../../asstes/user4.svg"
 
 
 const initialState={
@@ -16,7 +20,13 @@ const initialState={
     { teamName: "ساران فن آور گستران اطلس", id: 3, memberShip: 8, src: uplawImage },
 
   ],
-  selectedTeam:""
+  selectedTeam:"",
+   teamMember:[
+     {id:1,name:"محسن خانی"},
+     {id:2,name:"سارا کریمی صفت"},
+     {id:3,name:"سامان یوسفی"},
+     {id:4,name:"رضا حسین زاده"},
+   ]
 
 
 }
