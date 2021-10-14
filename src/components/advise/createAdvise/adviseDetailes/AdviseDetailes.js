2d9e6955989ab25ecf4d13c8cba529f0e99e2cdf
adib sadeghi <adibsadeghi7272@gmail.com>
2021-10-14 21:30:56 +0330

    Progress component was added to AdviseDetailes component

--- src/components/advise/createAdvise/adviseDetailes/AdviseDetailes.js
+++ src/components/advise/createAdvise/adviseDetailes/AdviseDetailes.js
@@ -1,17 +1,49 @@
-import React from 'react'
-import styles from "./AdviseDetailes.module.css"
-import FingerImage from "../../../common/fingerImgae/FingerImgae"
+import React, { useState } from "react";
+import styles from "./AdviseDetailes.module.css";
+import FingerImage from "../../../common/fingerImgae/FingerImgae";
+import Progress from "../../../common/contractHeader/ContractHeader";
 
 const AdviseDetailes = () => {
-    return (
-        <div>
-            <h3>/مشاوره</h3>
-        <FingerImage>
-            تو این صفحه میتونی تایم مشاوره رو با مشاورین آپلا ست بکنی و با خیال راحت دغدغه هات رو حل کنی
-        </FingerImage>
-            
-        </div>
-    )
-}
+  const [active, setActive] = useState("newAdvise");
+  const prevAdviseHandler = () => {
+    setActive("prevAdvise");
+  };
+  const newAdviseHandler = () => {
+    setActive("newAdvise");
+  };
+  return (
+    <div>
+      <h3>/مشاوره</h3>
+      <FingerImage>
+        تو این صفحه میتونی تایم مشاوره رو با مشاورین آپلا ست بکنی و با خیال راحت
+        دغدغه هات رو حل کنی
+      </FingerImage>
+      <div className={styles.advise_header}>
+        <p
+          onClick={newAdviseHandler}
+          className={active === "newAdvise" ? styles.active : null}
+        >
+          ست کردن تایم مشاوره
+        </p>
+        <p
+          onClick={prevAdviseHandler}
+          className={active === "prevAdvise" ? styles.active : null}
+        >
+          مشاوره های قبلی
+        </p>
+      </div>
+      <Progress
+        noneBox="noneBox"
+        stepOne="انتخاب موضوع مشاوره"
+        stepTow="انتخاب زمان شماوره"
+        opacityThree="0.5"
+        adjust={false}
+      />
+      <div>
+          
+      </div>
+    </div>
+  );
+};
 
-export default AdviseDetailes
+export default AdviseDetailes;
